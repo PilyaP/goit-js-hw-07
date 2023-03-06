e591ff8d78f7d8e4b11107dae5b519ec342fbf18
PilyaP <ppaexpert@gmail.com>
2023-03-06 17:32:37 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const paletteContainer = document.querySelector('.gallery');
+const imageMarkup = createGalleryMarkup(galleryItems); 
+
+paletteContainer.insertAdjacentHTML('beforeend', imageMarkup);
+
+function createGalleryMarkup(images) {
+  const markup = images.map(({ preview, original, description }) => { 
+    return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+        </a>
+      </div>
+    `;
+  })
+  .join('');
+  return markup;
+}
+
+let instance;
+
+paletteContainer.addEventListener('click', onPaletteContainerClk);
+
+function onPaletteContainerClk(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  
+  instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+  `);
+  instance.show();
+
+  // 
+  document.addEventListener("keydown", closePicture);
+}
+
+function closePicture(event) {
+  if (event.key === "Escape") {
+    document.removeEventListener("keydown", closePicture);
+    instance.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const paletteContainer = document.querySelector('.gallery');
+const imageMarkup = createGalleryMarkup(galleryItems); 
+
+paletteContainer.insertAdjacentHTML('beforeend', imageMarkup);
+
+function createGalleryMarkup(images) {
+  const markup = images.map(({ preview, original, description }) => { 
+    return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" 
+          src="${preview}" 
+          alt="${description}">
+        </a>
+      </div>
+    `;
+  })
+  .join('');
+  return markup;
+}
+const lightbox = new SimpleLightbox('.gallery a', {   captionsData: "alt",
+  captionDelay: 250, });
